ae8d42f134765ff937a13ba2726c92fe2991a150
Todd Demone <todd.demone.dev@gmail.com>
2022-04-07 17:33:36 +0000

    complete Rails 4.2 getting started

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  # by creating an instance of the Article (model) class, and saving it as an instance variable, we automatically pass that Model instance to the 'new' view
+  # The model adds some validation to each article object. If the validation raises an error, then the new view needs to know about it so it can print error messages. That's why we pass @article to the new view.
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
